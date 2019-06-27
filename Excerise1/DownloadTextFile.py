55a301cd3ddc176be9bd14eb6d917f4817a5f69b
Franco <c0demonkers@gmail.com>
2019-06-27 10:52:25 -0400

    just showing what happens when we try to induce an 404

--- Excerise1/DownloadTextFile.py
+++ Excerise1/DownloadTextFile.py
@@ -2,8 +2,19 @@
 #download ideas came from https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
 
 import urllib.request
-...
+
+''' when everything works according to plan (no 404)
 url = 'http://www.gutenberg.org/files/59812/59812-0.txt'
 response = urllib.request.urlopen(url)
 data = response.read()      # a `bytes` object
-text = data.decode('utf-8') # a `str`; this step can't be used if data is bina
+text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
+print(text)
+'''
+
+''' when we get a 404'''
+'''url = 'http://www.gutenberg.org/files/2/2-0.txt'
+response = urllib.request.urlopen(url)
+data = response.read()      # a `bytes` object
+text = data.decode('utf-8') # a `str`; this step can't be used if data is binary
+print(text)'''
+'''urllib.error.HTTPError: HTTP Error 404: Not Found''
