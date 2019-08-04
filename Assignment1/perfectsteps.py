76ae0907f6b75f6b921083701dde0018558e07bc
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-08-04 14:12:39 -0400

    x = mv completed

--- Assignment1/perfectsteps.py
+++ Assignment1/perfectsteps.py
@@ -1,7 +1,9 @@
 #1st algorithm in chapter 5
+# x = M v
 import numpy.matlib 
 import numpy as np 
 
+#this works up to the third step... IDK why
 
 def main():
     A = np.array([
@@ -10,13 +12,10 @@ def main():
         [1/3,0,0,1/2],
         [1/3,1/2,0,0]
         ])
-
     B = np.array([[1/4],[1/4],[1/4],[1/4]])
 
-    C = np.matmul(A,B) 
-    
-
-    print(C)
+    steps = np.matmul(np.power(A,2),B)
+    print(steps)
 
 
 main()
