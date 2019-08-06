fcae32280df667f9b95bcb6e7c7b50a8d5b38125
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-08-06 18:07:15 -0400

    the right way of doing it

--- Assignment1/matrixmulitplication.py
+++ Assignment1/matrixmulitplication.py
@@ -7,22 +7,22 @@ class page_rank (MRJob):
 
     def map_elements(self, _, line):
         row = line.split('|')
-        values = row[2].split(' ')
-        size = row[0].split('x')
+        values = row[1].split(' ')
         matrix_name = str(os.environ['mapreduce_map_input_file']).split('/')[-1]
         is_small_matrix = matrix_name == 'fivebyone.txt'
         values_len = len(values)
         for i in range(0,values_len):
             if is_small_matrix:
-                for j in range(0,int(size[0])):
-                    yield (j+1,(matrix_name,int(row[1]),int(values[i])))
+                    yield (i,(matrix_name,int(row[0]),int(values[i])))
             else:
-                yield(int(row[1]),(matrix_name,i,int(values[i])))
+                yield(int(row[0]),(matrix_name,i,int(values[i])))
         
 
 
     def reduce_elements(self, index, values):
-        yield(index, list(values))
+        list_of_values = list(values)
+        for rec in list_of_values:
+            yield(rec[0], (rec[1], rec[2]) )
 
     def steps(self):
         return [
