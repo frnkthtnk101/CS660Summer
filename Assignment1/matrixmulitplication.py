c5b3f1c3ecd6b80048e4919892f2f2793d3fd751
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-08-06 18:57:05 -0400

    I got it to work! I had the things fliped

--- Assignment1/matrixmulitplication.py
+++ Assignment1/matrixmulitplication.py
@@ -9,7 +9,7 @@ class page_rank (MRJob):
         row = line.split('|')
         values = row[1].split(' ')
         matrix_name = str(os.environ['mapreduce_map_input_file']).split('/')[-1]
-        is_small_matrix = matrix_name == 'fivebyone.txt'
+        is_small_matrix = matrix_name == 'fivebyfive.txt'
         values_len = len(values)
         for i in range(0,values_len):
             if is_small_matrix:
@@ -17,20 +17,24 @@ class page_rank (MRJob):
             else:
                 yield(int(row[0]),(matrix_name,i,int(values[i])))
         
-
-
     def reduce_elements(self, index, values):
         list_of_values = list(values)
-        for rec in list_of_values:
-            yield(rec[0], (rec[1], rec[2]) )
+        list_O_left = [i for i in list_of_values if i[0] == 'fivebyfive.txt']
+        list_O_rights = [i for i in list_of_values if i[0] == 'fivebyone.txt']
+        for left in list_O_left:
+            for right in list_O_rights:
+                result = int(left[2]) * int(right[2])
+                yield((left[1], right[1]), result) 
+    
 
     def steps(self):
         return [
             MRStep(
-                mapper=self.map_elements,
+                mapper = self.map_elements,
                 reducer = self.reduce_elements
             )
         ]
+        
 
 if __name__ == '__main__':
     page_rank.run()
