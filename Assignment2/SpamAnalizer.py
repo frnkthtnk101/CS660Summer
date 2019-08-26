5fe8b7308edee27cddf4076e59f67120347c3c43
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-08-26 19:23:34 -0400

    created an inverted index... checking how it works

--- Assignment2/SpamAnalizer.py
+++ Assignment2/SpamAnalizer.py
@@ -6,13 +6,17 @@ WORD_RE = re.compile(r"[a-z|A-Z]+")
 
 class spam_analizer(MRJob):
     def map_training_data(self, _, line):
-        message = line.split('\t')
-        for word in WORD_RE.findall(message[-1]):
-            yield [word.lower(), message[1]], 1
+        message = line.split(',')
+        message_length = len(message)
+        for i in range(1,message_length):
+            for word in WORD_RE.findall(message[i]):
+                yield [word.lower(), message[0]], 1
 
+    def combine_word_terms(self, term_type, count):
+        yield term_type ,sum(count)
 
     def steps(self):
-        return MRStep(mapper = self.map_training_data)
+        return MRStep(mapper = self.map_training_data, reducer = self.combine_word_terms)
 
 if __name__ == '__main__':
     spam_analizer.run()
