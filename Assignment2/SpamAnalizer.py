156eb967db5e7e7de715262041cb21adcd5067e8
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-08-26 19:17:15 -0400

    maping the training data

--- Assignment2/SpamAnalizer.py
+++ Assignment2/SpamAnalizer.py
@@ -2,13 +2,17 @@ from mrjob.job import MRJob
 from mrjob.step import MRStep
 import re
 
-WORD_RE = re.compile(r"[\w']+")
+WORD_RE = re.compile(r"[a-z|A-Z]+")
 
 class spam_analizer(MRJob):
+    def map_training_data(self, _, line):
+        message = line.split('\t')
+        for word in WORD_RE.findall(message[-1]):
+            yield [word.lower(), message[1]], 1
+
+
     def steps(self):
-        return [
-            MRStep()
-        ]
+        return MRStep(mapper = self.map_training_data)
 
 if __name__ == '__main__':
     spam_analizer.run()
