7d56313f0490c3aa2aeee051385242be5a8f8b15
frnkthtnk100 <fpettigrosso@holyfamily.edu>
2019-08-26 20:26:13 -0400

    got it where it can be consumable by older code

--- Assignment2/SpamAnalizer.py
+++ Assignment2/SpamAnalizer.py
@@ -3,6 +3,7 @@ from mrjob.step import MRStep
 import re
 
 WORD_RE = re.compile(r"[a-z|A-Z]+")
+TXT_MESSAGE = "I love school"
 
 class spam_analizer(MRJob):
     def map_training_data(self, _, line):
@@ -12,11 +13,34 @@ class spam_analizer(MRJob):
             for word in WORD_RE.findall(message[i]):
                 yield [word.lower(), message[0]], 1
 
-    def combine_word_terms(self, term_type, count):
-        yield term_type ,sum(count)
+    def combine_word_terms_ham(self, term_type, count):
+        yield None, [term_type[0], term_type[1], sum(count)]
+
+    def reducer_make_dictionary(self, _, term_type_count):
+        P = [{},{}]
+        for term, classification, count in term_type_count:
+            if(classification == "ham"):
+                P[0][term] = count
+            else:
+                P[1][term] = count
+        yield P, None
+    
+    #def reducer_do_calculation_ham(self, ham_dictionary, _):
+            
+        
 
     def steps(self):
-        return MRStep(mapper = self.map_training_data, reducer = self.combine_word_terms)
+        return [
+            #this is the training data
+            MRStep(
+                mapper = self.map_training_data,
+                reducer = self.combine_word_terms_ham
+                ),
+            #now we need to calculate
+            MRStep(
+                reducer = self.reducer_make_dictionary
+            )
+            ]
 
 if __name__ == '__main__':
     spam_analizer.run()
